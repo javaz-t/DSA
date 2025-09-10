432538ec64ea87fc9d30a23208c7e2d53477fb94
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2025-09-10 13:26:58 +0530

    Time: 133 ms (5.81%) | Memory: 20.8 MB (54.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -6,7 +6,9 @@ class Solution(object):
             area = min(height[left], height[right]) * (right - left)  
             total = max(total, area)  
             if height[left] > height[right]:
+                area = height[right] * (right - left)  
                 right = right - 1
             else:
+                area = height[right] * (right - left)
                 left = left + 1
         return total
