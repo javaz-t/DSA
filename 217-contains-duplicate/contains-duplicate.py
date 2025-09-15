b36ecc5388774adf04f064564b56a82c5e72dda5
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2025-09-15 14:31:02 +0530

    Time: 23 ms (32.36%) | Memory: 25.9 MB (47.08%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,19 +1,26 @@
 class Solution(object):
-   def containsDuplicate(self,nums):
+    def containsDuplicate(self, nums):
+        data = set()
+        for n in nums:
+            if n in data:       # ✅ use `in` instead of contains
+                return True
+            else:
+                data.add(n)     # ✅ use `add()` for set
+        return False
     
         # #set use to remove the duplicate items 
         # setCount=set(nums)
         # #checkign the values lenth are same or not 
         # return len(setCount)!=len(nums)
-    l,r =(0,1)
-    nums.sort()
-    if len(nums)==1:
-        return False
-    while(r<len(nums)):
-        if nums[l]==nums[r]:
-            return True
-        else:
-            l+=1
-            r+=1
-    return False
+    # l,r =(0,1)
+    # nums.sort()
+    # if len(nums)==1:
+    #     return False
+    # while(r<len(nums)):
+    #     if nums[l]==nums[r]:
+    #         return True
+    #     else:
+    #         l+=1
+    #         r+=1
+    # return False
         
