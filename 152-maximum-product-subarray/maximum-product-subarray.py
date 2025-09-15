c11f098693b792851718e99501d300c510794d64
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2025-09-15 16:25:28 +0530

    Time: 11 ms (53.62%) | Memory: 12.7 MB (85.95%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxProduct(self, nums ):
-        res = nums[0]                
+        res =max(nums)                
         curmin, curmax = 1, 1       
 
         for n in nums:
+            if n==0:
+                curmin,curmax=1,1
+                continue
             tmp = curmax * n
             curmax = max(tmp, curmin*n, n)
             curmin = min(tmp, curmin*n, n)
