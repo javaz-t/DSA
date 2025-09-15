8cf2f73ce770de1993977447d176b6ff71f76a2e
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2025-09-15 19:26:45 +0530

    Time: 7 ms (48.94%) | Memory: 13 MB (35.23%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        #[2,7,11,15], target = 9
+        l,r=0,len(nums)-1
+        while(l<r):
+            sumVal=nums[r]+nums[l]
+            if target==sumVal:
+                return [l+1,r+1]
+            elif target<sumVal:#9<17  <13
+                r-=1
+            else:
+                l+=1
+        return [l+1,r+1]
+
+        
