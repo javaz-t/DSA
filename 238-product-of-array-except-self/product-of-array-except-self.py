b931d9bbf440ec10c6e4dacfef36eccc21310abc
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2025-09-22 16:08:55 +0530

    Time: 57 ms (14.22%) | Memory: 21.2 MB (36.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,22 @@
 class Solution(object):
-    def productExceptSelf(self, nums):
-        prefix=[]
-        postfix=[]
-        result=[]
-        val=1
-
-        for i in range( len(nums)):
-            val=val*nums[i]
-            prefix.append(val)
-        #print(prefix)
-        val=1
-        for i in range(len(nums)-1,-1,-1):
-            val=val*nums[i]
-            postfix.insert(0, val)
-        #print(postfix)
-        
-        for i in range(len(nums)):
-            prefixVal = 1 if i == 0 else prefix[i - 1]
-            postfixVal = 1 if i == len(nums) - 1 else postfix[i + 1]
-            result.append(prefixVal * postfixVal)
+    def productExceptSelf(self, nums): 
+        result=[] 
+        for i in range( len(nums)):            
+            val=1 if i==0 else val*nums[i-1]
+            result.append(val)
+        #print(result)
+        post=1
+        for i in range( len(nums)-1,-1,-1):
+           
+            val=post*result[i] if i==len(nums)-1 else post*result[i]*nums[i+1]
+            if i!=len(nums)-1:
+                post*=nums[i+1]
+            result[i]=val
         #print(result)
         return result
+        
+         
+        
+             
+                    
+print(Solution().productExceptSelf([1,2,3,4]))
