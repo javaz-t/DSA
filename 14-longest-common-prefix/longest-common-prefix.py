c4b7f113e66ec3c0bafb39d5ba2bbc3f6f8c205b
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2026-06-04 13:04:39 +0530

    Time: 4 ms (15.88%) | Memory: 12.5 MB (33.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,7 +4,7 @@ class Solution(object):
         for i in range(len(strs[0])): # first word len
             tempVal = strs[0][i]
             for x in strs:
-                if i == len(x) or x[i]!=tempVal:
+                if i == len(x) or x[i]!=tempVal:#check reach the min word length 
                     return output
             output = output + tempVal
         return output
