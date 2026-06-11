556d0cb77c6c8edc90f6684f1911b7c916f2e9f2
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2026-06-11 23:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (53.70%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def removeElement(self, nums, val):
         k=0
-        result =[]
-        for x in nums:
-            if x==val:
-                result.append('_')
-            else:
-                result.insert(0,x)
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
                 k=k+1
-        nums[:] = result
+           
         return k
 
        
