d3f52dacc325f4b8b7a11762c12af17d903c09af
Abdul Javaz T <147338963+javaz-t@users.noreply.github.com>
2026-07-15 13:07:46 +0530

    Time: 3 ms (86.08%) | Memory: 13.5 MB (73.20%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,12 +2,19 @@
 
 class Solution:
     def majorityElement(self, nums):
-        result = {}
-        for   x in  nums :
-            if x in result:
-                result[x] = result[x] + 1
+        mE=-1
+        count =-1
+        for x in nums:
+            if mE==x:
+                count+=1
             else:
-                result[x] = 1
-        return max(result,key=result.get)
-
+                
+                if count <1:
+                    mE=x
+                    count =0
+                else:
+                    count-=1
+        return mE
+         
+         
  
